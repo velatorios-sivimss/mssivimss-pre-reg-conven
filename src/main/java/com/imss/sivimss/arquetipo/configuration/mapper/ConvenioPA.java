ee3d2649bb09898335c21e79280fde0702f4d0e1
galo14 <yo123garcia@gmail.com>
2024-02-08 13:54:08 -0600

    se modifica el servicio de consulta de detalle convenios pf por empresa, ahora solo son 2 secciones 1 empresa y solicitantes 2 beneficiarios

--- src/main/java/com/imss/sivimss/arquetipo/configuration/mapper/ConvenioPA.java
+++ src/main/java/com/imss/sivimss/arquetipo/configuration/mapper/ConvenioPA.java
@@ -58,8 +58,8 @@ public interface ConvenioPA {
 		+ "		DATE_FORMAT(SP.FEC_NAC, '%d/%m/%Y') AS fecNacimiento, "
 		+ "		SP2.DES_PAIS AS pais, "
 		+ "		SP.ID_PAIS AS idPais, "
-		+ "		se.DES_ESTADO AS lugarNac, "
-		+ "		SP.ID_ESTADO AS idLugarNac, "
+		+ "		IFNULL(se.DES_ESTADO,'') AS lugarNac, "
+		+ "		IFNULL(SP.ID_ESTADO,'') AS idLugarNac, "
 		+ "		SP.REF_TELEFONO AS telCelular, "
 		+ "		SP.REF_TELEFONO_FIJO AS telFijo, "
 		+ "		SP.REF_CORREO AS correo, "
@@ -74,19 +74,20 @@ public interface ConvenioPA {
 		+ "		stpm.DES_TIPO_PAGO_MENSUAL AS numPagos, "
 		+ "		SP3.REF_PAQUETE_NOMBRE AS nomPaquete, "
 		+ "		SPS.IND_TITULAR_SUBSTITUTO AS titularSust, "
-		+ "		SPS.ID_TITULAR_SUBSTITUTO AS idTitularSust, "
-		+ "		SPS.ID_BENEFICIARIO_1 AS beneficiario1, "
-		+ "		SPS.ID_BENEFICIARIO_2 AS beneficiario2, "
-		+ "		SPS.IND_PROMOTOR AS gestionPromotor , SPS.IND_ACTIVO activo"
+		+ "		IFNULL(SPS.ID_TITULAR_SUBSTITUTO,0) AS idTitularSust, "
+		+ "		IFNULL(SPS.ID_BENEFICIARIO_1,0) AS beneficiario1, "
+		+ "		IFNULL(SPS.ID_BENEFICIARIO_2,0) AS beneficiario2, "
+		+ "		SPS.IND_PROMOTOR AS gestionPromotor , SPS.IND_ACTIVO activo, "
+		+ "		SP.ID_PERSONA idPersonaContratante, SD.ID_DOMICILIO idDomicilio "
 		+ "	FROM "
 		+ "		SVT_PLAN_SFPA SPS "
 		+ "	JOIN SVC_CONTRATANTE sc ON "
 		+ "		sc.ID_CONTRATANTE = SPS.ID_TITULAR "
 		+ "	JOIN SVC_PERSONA SP ON "
 		+ "		SP.ID_PERSONA = sc.ID_PERSONA "
-		+ "	JOIN SVC_PAIS SP2 ON "
+		+ "	LEFT JOIN SVC_PAIS SP2 ON "
 		+ "		SP2.ID_PAIS = SP.ID_PAIS "
-		+ "	JOIN SVC_ESTADO se ON "
+		+ "	LEFT JOIN SVC_ESTADO se ON "
 		+ "		se.ID_ESTADO = SP.ID_ESTADO "
 		+ "	JOIN SVT_DOMICILIO SD ON "
 		+ "		SD.ID_DOMICILIO = sc.ID_DOMICILIO "
@@ -137,7 +138,7 @@ public interface ConvenioPA {
 				+ " JOIN SVT_DOMICILIO SD ON "
 				+ " 	SD.ID_DOMICILIO = stb.ID_DOMICILIO "
 				+ "WHERE stb.ID_TITULAR_BENEFICIARIOS =  #{idBeneficiario} " )
-	public BenefXPA consultaBeneficiariosConvenioPA( @Param("idBeneficiario") Integer idBeneficiario );
+	public BenefXPA consultaBeneficiariosConvenioPA( @Param("idBeneficiario") Long idBeneficiario );
 
 @Select("SELECT TIT.ID_TITULAR_BENEFICIARIOS idBeneficiario, " + 
 		"		TIT.REF_PERSONA referencia, " + 
@@ -173,5 +174,5 @@ public interface ConvenioPA {
 		"INNER	JOIN SVC_ESTADO se ON se.ID_ESTADO = PER.ID_ESTADO " + 
 		"INNER	JOIN SVT_DOMICILIO SD ON SD.ID_DOMICILIO = TIT.ID_DOMICILIO  " + 
 		"WHERE	TIT.ID_TITULAR_BENEFICIARIOS = #{idTitularSus} ")
-	public BenefXPA consultaTitularSust( @Param("idTitularSus") Integer idTitularSus );
+	public BenefXPA consultaTitularSust( @Param("idTitularSus") Long idTitularSus );
 }
